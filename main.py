9412fc82df478ef0ee95770b6728a0e5d85774a0
Harshit2786 <harshit.2002.singh@gmail.com>
2024-11-28 01:57:12 +0530

    added cors

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
 from routers import quiz
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:8081",  # React Native development server
+    "http://127.0.0.1:8081",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allow only specific origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
 app.include_router(quiz.router)
